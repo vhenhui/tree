1fc6ce5b70f75f13a36cf83c15071c7356a3aefa
vhenhui <vhenhui@163.com>
2023-12-16 12:25:11 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -9,7 +9,7 @@ export default siteConfig({
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
+  name: '境随心转 心想事成',
   domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
   author: 'Travis Fischer',
 
